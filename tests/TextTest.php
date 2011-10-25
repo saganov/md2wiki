921ab061b7e003120bb2b13407ce2f453bcbc055
Max Tsepkov <max@garygolden.me>
2011-10-26 01:52:20 +0600

    TextTest: run ->getHtml() before ->__toString()
    This way we can know which exception is thrown,
    cause exceptions cannot be processed within __toString()

--- tests/TextTest.php
+++ tests/TextTest.php
@@ -42,12 +42,12 @@ class TextTest extends PHPUnit_Framework_TestCase
         $text = new Markdown_Text(self::$_md['syntax']);
         $this->assertEquals(
             self::$_html['syntax'],
-            (string) $text,
+            $text->getHtml(),
             'Resulting html does not match produced by original Markdown.pl 1.0.1'
         );
         $this->assertEquals(
             self::$_html['syntax'],
-            $text->getHtml(),
+            (string) $text,
             'Resulting html does not match produced by original Markdown.pl 1.0.1'
         );
     }
