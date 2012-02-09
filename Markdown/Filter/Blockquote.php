83ab07210f1720925abde24ecefac1db968381af
Max Tsepkov <max@garygolden.me>
2012-02-10 03:15:47 +0600

    Blockquote: filter implementation
    Introduces slight difference from original markdown, thus
    byte to byte comparison won't work for tests.

--- Markdown/Filter/Blockquote.php
+++ Markdown/Filter/Blockquote.php
@@ -23,10 +23,80 @@
 
 require_once __DIR__ . '/../Filter.php';
 
+/**
+ * Translates email-style blockquotes to <blockquote>
+ *
+ * Rules from markdown definition:
+ *
+ *   *  blockquote is indicated by < at the start of line
+ *   *  blockquotes can be nested
+ *   *  lazy blockquotes are allowed
+ *
+ * Blockquote ends with \n\n
+ *
+ * @author Max Tsepkov <max@garygolden.me>
+ *
+ */
 class Markdown_Filter_Blockquote extends Markdown_Filter
 {
     public function transform($text)
     {
+        foreach($this->searchQuotes($text) as $quote)
+        {
+            $text = str_replace($quote, $this->transformQuote($quote), $text);
+        }
+
         return $text;
+    }
+
+    /**
+     * Search markdown for quotes and returns it untouched.
+     *
+     * @param array $quotes
+     */
+    protected function searchQuotes($text)
+    {
+        $quotes = array();
+
+        $inQuote = false;
+        $len = strlen($text);
+        for ($pos = 0; $pos < $len; $pos++) {
+            if (!$inQuote) {
+                if ($text[$pos] == '>' && ($pos == 0 || $text[$pos-1] == "\n")) {
+                    $inQuote  = true;
+                    $quotes[] = '';
+                    $quote    =& $quotes[count($quotes) - 1];
+                }
+            }
+
+            if ($inQuote) {
+                if ($text[$pos] == "\n" && $text[$pos-1] == "\n") {
+                    $inQuote = false;
+                }
+                else {
+                    $quote .= $text[$pos];
+                }
+            }
+        }
+
+        return $quotes;
+    }
+
+    /**
+     * Recursive function takes a signle markdown quote
+     * and returns its html equivalent.
+     *
+     * @param string
+     * @return string
+     */
+    protected function transformQuote($text)
+    {
+        $text = preg_replace('/^\s*>\s*/m', '', $text);
+
+        foreach ($this->searchQuotes($text) as $quote) {
+            $text = str_replace($quote, $this->transformQuote($quote), $text);
+        }
+
+        return "<blockquote>\n" . $text . "</blockquote>\n";
     }
 }
