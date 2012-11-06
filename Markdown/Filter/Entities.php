294a853bf16aa27c39322cc81ebae51687c31536
Max Tsepkov <max@garygolden.me>
2012-11-06 17:51:21 +0400

    analyze regexps in entities

--- Markdown/Filter/Entities.php
+++ Markdown/Filter/Entities.php
@@ -69,10 +69,10 @@ class Filter_Entities extends Filter
             );
 
             // escape & outside of html entity
-            $line = preg_replace('/&(?![A-z]+;)/u', '&amp;', $line);
+            $line = preg_replace('/&(?![A-z]+;)/uS', '&amp;', $line);
 
             // escape < outside of html tag
-            $line = preg_replace('/<(?![A-z\\/])/u', '&lt;', $line);
+            $line = preg_replace('/<(?![A-z\\/])/uS', '&lt;', $line);
         }
     }
 }
